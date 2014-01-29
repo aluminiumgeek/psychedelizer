5fe9f878cf630c5d8a2cd5545cf88393ef8a2fb3
aluminiumgeek <mihail265@gmail.com>
2014-01-29 08:19:08 +0400

    undefined text issue

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,7 +1,9 @@
 
 app.filter('remove_ext', function() {
     return function(text) {
-        return text.substr(0, text.lastIndexOf('.'));
+        if (text !== undefined) {
+            return text.substr(0, text.lastIndexOf('.'));
+        }
     };
 });
 
